0f17c65683f6c2389030c26e3ef6979125b163cb
Michael <mib@michael-baumgaertner.de>
2019-01-05 21:54:16 +0100

    update copyright notice

--- Signaling.cpp
+++ Signaling.cpp
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Operating Support code for Arduino or ESP8266 projects.
 //
-// Copyright (c) 2017/2018 Michael Baumgärtner
+// Copyright (c) 2017-2019 Michael Baumgärtner
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
